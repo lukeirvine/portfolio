5fd495c696927d5f6bfb7b4dce4c6f0217d14194
Luke Irvine <lukeirvinepiano@gmail.com>
2021-11-12 00:58:13 -0800

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Homepage from './components/pages/homepage/Homepage';
 import Portfolio from './components/pages/portfolio/Portfolio';
@@ -7,13 +7,13 @@ import Error from "./components/pages/error/Error";
 
 const App = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
           <Switch>
               <Route path="/" component={Homepage} exact/>
               <Route path="/apps" component={Portfolio} exact/>
               <Route render={(props) => <Error {...props}/>}/>
           </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
