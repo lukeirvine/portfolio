7f11158e0b674cf4d6c133c8e6b930986dbda8ff
Luke Irvine <lukeirvinepiano@gmail.com>
2022-02-01 17:01:10 -0800

    added homepage picture links

---
+++ src/resources/images.js
@@ -0,0 +1,39 @@
+export const imgs = {
+    music: {
+        swingDance: 'https://i.imgur.com/5NLIcPa.png',
+        winery: 'https://i.imgur.com/pndhVne.jpg',
+        villageHall: 'https://i.imgur.com/BgHdC1a.jpg',
+        allCreatures: 'https://i.imgur.com/82V6TMg.jpg',
+        practiceRoom: 'https://i.imgur.com/ZSvtToV.png',
+        botb: 'https://i.imgur.com/JCS54MC.jpg',
+        springJam: 'https://i.imgur.com/7ROLluW.jpg',
+        neverLost: 'https://i.imgur.com/aDDNqC8.jpg',
+        highlightsConcert: 'https://i.imgur.com/rpgMKyN.jpg',
+        margieOtt: 'https://i.imgur.com/0UQX896.jpg',
+        jazzTrio: 'https://i.imgur.com/KXPJDyz.png',
+        treeFarm: 'https://i.imgur.com/Jgpg8QS.jpg',
+        vespersOutside: 'https://i.imgur.com/aSoXlCI.jpg',
+        vespersPracticeRoom: 'https://i.imgur.com/7WdvuLe.jpg'
+    },
+    goaltop: {
+        homepage: 'https://i.imgur.com/Zjsf9ee.jpg',
+        dashboard: 'https://i.imgur.com/U2KPCeZ.png'
+    },
+    surveyCounter: {
+        reports: 'https://i.imgur.com/TLmtJ9t.png',
+        homepage: 'https://i.imgur.com/Kcv4Iml.png'
+    },
+    mealDash: {
+        phone: 'https://i.imgur.com/pN6d4aU.jpg',
+        dash: 'https://i.imgur.com/sJXxmQ3.png'
+    },
+    smallGroups: {
+        groupCategory: 'https://i.imgur.com/ccimp3e.png',
+        startGroup: 'https://i.imgur.com/TdoqfuY.png',
+        groupCards: 'https://i.imgur.com/Diw5KSJ.png',
+        homepage: 'https://i.imgur.com/cT6qzp3.jpg'
+    },
+    midi: {
+        gm7: 'https://i.imgur.com/TsIxURi.png'
+    }
+}
